1ab3839cbcdb782e9730b2a7f49958e0a004fe14
JW-Kim <jw4005.kim@gmail.com>
2018-08-08 08:55:45 +0900

    sample

---
+++ src/Components/Sample/BasicComponentsTextInput.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { TextInput } from 'react-native';
+
+export default class basicComponentsTextInput extends Component {
+    constructor(props){
+        super(props)
+        this.state = {text: 'basicComponentsTextInput'}
+    }
+
+    render() {
+        return (
+            <TextInput
+                style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+                onChangeText={(text) => this.setState({text})}
+                value={this.state.text}
+            />
+        )
+    }
+
+}
