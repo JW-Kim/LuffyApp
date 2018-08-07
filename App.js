a5a23a1df08f46b330e3d03ca0e605043bd10696
JW-Kim <jw4005.kim@gmail.com>
2018-08-08 08:53:51 +0900

    test

--- App.js
+++ App.js
@@ -3,17 +3,18 @@ import { View, Text } from 'react-native';
 import { createStackNavigator, createTabNavigator } from 'react-navigation';
 import Modal from './src/Components/Modal.js'
 import Home from './src/Components/Home.js'
+import BasicComponents from './src/Components/Sample/BasicComponents.js'
 import Detail from './src/Components/Detail.js'
 
 const MainStack = createTabNavigator(
-  {
-    Home: {
-      screen: Home,
+    {
+        Home: Home,
+        Detail:  Detail,
+        BasicComponents : BasicComponents
     },
-    Detail:  {
-      screen: Detail,
-    },
-  }
+    {
+       initialRouteName : 'Home'
+    }
 );
 
 const RootStack = createStackNavigator(
