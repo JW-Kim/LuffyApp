a35ed242ed07838e8cb5aaaa27ba2c2fc7520fb7
JW-Kim <jw4005.kim@gmail.com>
2018-08-08 14:49:25 +0900

    app

--- App.js
+++ App.js
@@ -4,13 +4,15 @@ import { createStackNavigator, createTabNavigator } from 'react-navigation';
 import Modal from './src/Components/Modal.js'
 import Home from './src/Components/Home.js'
 import BasicComponents from './src/Components/Sample/BasicComponents.js'
+import UserInterface from './src/Components/Sample/UserInterface.js'
 import Detail from './src/Components/Detail.js'
 
 const MainStack = createTabNavigator(
     {
         Home: Home,
         Detail:  Detail,
-        BasicComponents : BasicComponents
+        BasicComponents : BasicComponents,
+        UserInterface : UserInterface
     },
     {
        initialRouteName : 'Home'
