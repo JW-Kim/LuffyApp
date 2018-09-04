2b22da575791eb633f4c5fbfa7cb37d1f9800d2b
JW-KIM <jw4005.kim@gmail.com>
2018-09-05 00:52:56 +0900

    수정

--- src/Components/DiaryDtl.js
+++ src/Components/DiaryDtl.js
@@ -60,147 +60,212 @@ export default class DiaryDtl extends Component {
             ></ModalHeader>
             <View style={{height:Dimensions.get('window').height-148, marginLeft:18, marginRight:18, marginTop:18, backgroundColor:'white'}}>
                 <ScrollView style={{padding : 20}}>
-                    <View>
+                    <View style={{marginBottom:20}}>
                         <Image width={Dimensions.get('window').width}
                             source={require('../../assets/images/B612_20180814_195816_395.jpg')}
                         />
                     </View>
                     <View style={styles.checkContent}>
-                        <Text style={{width: 70}}>기분</Text>
+                        <Text style={{width: 70, fontSize: 15, fontWeight:'800'}}>기분</Text>
                         <CheckBox
                           title='좋음'
+                          size={20}
+                          checkedColor={'#33cc33'}
                           containerStyle={styles.checkBox}
                           checked={this.state.checked}
+                          textStyle={{fontSize: 15, fontWeight:'100'}}
                         />
                         <CheckBox
                           title='보통'
-                           containerStyle={styles.checkBox}
+                          size={20}
+                          checkedColor={'#ff8c00'}
+                          containerStyle={styles.checkBox}
                           checked={this.state.checked}
+                          textStyle={{fontSize: 15, fontWeight:'100'}}
                         />
                         <CheckBox
                           title='나쁨'
-                           containerStyle={styles.checkBox}
+                          size={20}
+                          checkedColor={'#ff471a'}
+                          containerStyle={styles.checkBox}
                           checked={this.state.checked}
+                          textStyle={{fontSize: 15, fontWeight:'100'}}
                         />
                     </View>
                     <View style={styles.checkContent}>
-                        <Text style={{width: 70}}>건강</Text>
+                        <Text style={{width: 70, fontSize: 15, fontWeight:'800'}}>건강</Text>
                         <CheckBox
                           title='좋음'
+                          size={20}
+                          checkedColor={'#33cc33'}
                           containerStyle={styles.checkBox}
                           checked={this.state.checked}
+                          textStyle={{fontSize: 15, fontWeight:'100'}}
                         />
                         <CheckBox
                           title='보통'
-                           containerStyle={styles.checkBox}
+                          size={20}
+                          checkedColor={'#ff8c00'}
+                          containerStyle={styles.checkBox}
                           checked={this.state.checked}
+                          textStyle={{fontSize: 15, fontWeight:'100'}}
                         />
                         <CheckBox
                           title='나쁨'
+                          size={20}
+                          checkedColor={'#ff471a'}
                           containerStyle={styles.checkBox}
                           checked={this.state.checked}
+                          textStyle={{fontSize: 15, fontWeight:'100'}}
                         />
                     </View>
                     <View style={styles.checkContent}>
                         <View style={{width:70, alignItems:'flex-start'}}>
-                             <Text style={{width: 70}}>열</Text>
+                             <Text style={{width: 70, fontSize: 15, fontWeight:'800'}}>열</Text>
                         </View>
                         <View>
                             <View style={{flexDirection:'row'}}>
                                 <CheckBox
                                   title='좋음'
+                                  size={20}
+                                  checkedColor={'#33cc33'}
                                   containerStyle={styles.checkBox}
                                   checked={this.state.checked}
+                                  textStyle={{fontSize: 15, fontWeight:'100'}}
                                 />
                                 <CheckBox
                                   title='보통'
+                                  size={20}
+                                  checkedColor={'#ff8c00'}
                                   containerStyle={styles.checkBox}
                                   checked={this.state.checked}
+                                  textStyle={{fontSize: 15, fontWeight:'100'}}
                                 />
                                 <CheckBox
                                   title='나쁨'
+                                  size={20}
+                                  checkedColor={'#ff471a'}
                                   containerStyle={styles.checkBox}
                                   checked={this.state.checked}
+                                  textStyle={{fontSize: 15, fontWeight:'100'}}
                                 />
                             </View>
-                            <TextInput></TextInput>
+                            <View style={{paddingLeft:10, paddingRight:20}}>
+                                <TextInput></TextInput>
+                            </View>
                         </View>
                     </View>
                     <View style={styles.checkContent}>
-                        <Text style={{width: 70}}>아침식사</Text>
+                        <Text style={{width: 70, fontSize: 15, fontWeight:'800'}}>아침식사</Text>
                         <CheckBox
                           title='좋음'
+                          size={20}
+                          checkedColor={'#33cc33'}
                           containerStyle={styles.checkBox}
                           checked={this.state.checked}
+                          textStyle={{fontSize: 15, fontWeight:'100'}}
                         />
                         <CheckBox
                           title='보통'
+                          size={20}
+                          checkedColor={'#ff8c00'}
                           containerStyle={styles.checkBox}
                           checked={this.state.checked}
+                          textStyle={{fontSize: 15, fontWeight:'100'}}
                         />
                         <CheckBox
                           title='나쁨'
+                          size={20}
+                          checkedColor={'#ff471a'}
                           containerStyle={styles.checkBox}
                           checked={this.state.checked}
+                          textStyle={{fontSize: 15, fontWeight:'100'}}
                         />
                     </View>
                     <View style={styles.checkContent}>
-                        <Text style={{width: 70}}>점심식사</Text>
+                        <Text style={{width: 70, fontSize: 15, fontWeight:'800'}}>점심식사</Text>
                         <CheckBox
                           title='좋음'
+                          size={20}
+                          checkedColor={'#33cc33'}
                           containerStyle={styles.checkBox}
                           checked={this.state.checked}
+                          textStyle={{fontSize: 15, fontWeight:'100'}}
                         />
                         <CheckBox
                           title='보통'
+                          size={20}
+                          checkedColor={'#ff8c00'}
                           containerStyle={styles.checkBox}
                           checked={this.state.checked}
+                          textStyle={{fontSize: 15, fontWeight:'100'}}
                         />
                         <CheckBox
                           title='나쁨'
+                          size={20}
+                          checkedColor={'#ff471a'}
                           containerStyle={styles.checkBox}
                           checked={this.state.checked}
+                          textStyle={{fontSize: 15, fontWeight:'100'}}
                         />
                     </View>
                     <View style={styles.checkContent}>
-                        <Text style={{width: 70}}>저녁식사</Text>
+                        <Text style={{width: 70, fontSize: 15, fontWeight:'800'}}>저녁식사</Text>
                         <CheckBox
                           title='좋음'
+                          size={20}
+                          checkedColor={'#33cc33'}
                           containerStyle={styles.checkBox}
                           checked={this.state.checked}
+                          textStyle={{fontSize: 15, fontWeight:'100'}}
                         />
                         <CheckBox
                           title='보통'
+                          size={20}
+                          checkedColor={'#ff8c00'}
                           containerStyle={styles.checkBox}
                           checked={this.state.checked}
+                          textStyle={{fontSize: 15, fontWeight:'100'}}
                         />
                         <CheckBox
                           title='나쁨'
+                          size={20}
+                          checkedColor={'#ff471a'}
                           containerStyle={styles.checkBox}
                           checked={this.state.checked}
+                          textStyle={{fontSize: 15, fontWeight:'100'}}
                         />
                     </View>
                     <View style={styles.checkContent}>
-                        <Text style={{width: 70}}>배변</Text>
+                        <Text style={{width: 70, fontSize: 15, fontWeight:'800'}}>배변</Text>
                         <View>
                             <View style={{flexDirection:'row'}}>
                                 <CheckBox
                                   title='좋음'
+                                  size={20}
+                                  checkedColor={'#33cc33'}
                                   containerStyle={styles.checkBox}
                                   checked={this.state.checked}
+                                  textStyle={{fontSize: 15, fontWeight:'100'}}
                                 />
                                 <CheckBox
                                   title='보통'
+                                  size={20}
+                                  checkedColor={'#ff8c00'}
                                   containerStyle={styles.checkBox}
                                   checked={this.state.checked}
+                                  textStyle={{fontSize: 15, fontWeight:'100'}}
                                 />
                                 <CheckBox
                                   title='나쁨'
+                                  size={20}
+                                  checkedColor={'#ff471a'}
                                   containerStyle={styles.checkBox}
                                   checked={this.state.checked}
+                                  textStyle={{fontSize: 15, fontWeight:'100'}}
                                 />
                             </View>
-                            <View style={{flexDirection:'row', alignItems:'center'}}>
+                            <View style={{flexDirection:'row', alignItems:'center', paddingLeft:10, paddingRight:20}}>
                                 <TextInput style={{flex:0.2, marginRight:3}}></TextInput>
                                 <Text style={{width:15, marginRight:10}}>회</Text>
                                 <TextInput style={{flex:0.9}}></TextInput>
@@ -208,30 +273,39 @@ export default class DiaryDtl extends Component {
                         </View>
                     </View>
                     <View style={styles.checkContent}>
-                        <Text style={{width: 70}}>수면</Text>
+                        <Text style={{width: 70, fontSize: 15, fontWeight:'800'}}>수면</Text>
                         <CheckBox
                           title='좋음'
+                          size={20}
+                          checkedColor={'#33cc33'}
                           containerStyle={styles.checkBox}
                           checked={this.state.checked}
+                          textStyle={{fontSize: 15, fontWeight:'100'}}
                         />
                         <CheckBox
                           title='보통'
+                          size={20}
+                          checkedColor={'#ff8c00'}
                           containerStyle={styles.checkBox}
                           checked={this.state.checked}
+                          textStyle={{fontSize: 15, fontWeight:'100'}}
                         />
                         <CheckBox
                           title='나쁨'
+                          size={20}
+                          checkedColor={'#ff471a'}
                           containerStyle={styles.checkBox}
                           checked={this.state.checked}
+                          textStyle={{fontSize: 15, fontWeight:'100'}}
                         />
                     </View>
-                    <View style={{flexDirection: 'row', alignItems: 'center', paddingLeft:5, marginTop:20}}>
-                        <Text style={{width: 70}}>제목</Text>
+                    <View style={styles.title}>
+                        <Text style={{width: 70, fontSize: 15, fontWeight:'800'}}>제목</Text>
                         <TextInput style={{flex:1}}>
                         </TextInput>
                     </View>
                     <View style={styles.checkContent}>
-                        <Text style={{width: 70}}>내용</Text>
+                        <Text style={{width: 70, fontSize: 15, fontWeight:'800'}}>내용</Text>
                         <TextInput style={{flex:1}}
                             numberOfLines={10}
                             multiline={true}
@@ -272,11 +346,17 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         paddingLeft:5,
-        justifyContent: 'space-between'
+        paddingRight:10,
+        justifyContent: 'space-between',
+        borderLeftWidth: 1,
+        borderTopWidth: 1,
+        borderRightWidth: 1,
+        borderColor: '#cccccc'
     },
     
     checkBox : {
-        width:70, 
+        width:68,
+        alignItems : 'center',
         backgroundColor:'white', 
         margin:0, 
         borderWidth:0
@@ -292,5 +372,15 @@ const styles = StyleSheet.create({
         borderColor: '#9B9B9B',
         justifyContent: 'center',
         alignItems: 'center'
+    },
+
+    title : {
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingLeft:5,
+        borderLeftWidth: 1,
+        borderTopWidth: 1,
+        borderRightWidth: 1,
+        borderColor: '#cccccc'
     }
 })
