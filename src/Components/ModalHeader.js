db34353d21a490a68441e089f3b416842b649a56
JW-Kim <jw4005.kim@gmail.com>
2018-09-18 18:11:53 +0900

    Update ModalHeader.js

--- src/Components/ModalHeader.js
+++ src/Components/ModalHeader.js
@@ -1,12 +1,18 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Button} from 'react-native';
 
 export default class ModalHeader extends Component {
 
   render(){
     return(
         <View style={styles.total}>
+            <View style={{width:30, backgroundColor : '#484f4f'}}></View>
             <Text style={styles.titleText}>{this.props.title}</Text>
+            <Button
+                style={{width:30}}
+                onPress={() => this.props.goEvent(this)}
+                title={this.props.buttonTitle}
+            />
         </View>
     )
   }
@@ -17,7 +23,8 @@ const styles = StyleSheet.create({
         height : 40,
         backgroundColor : '#484f4f',
         alignItems : 'center',
-        justifyContent : 'center'
+        justifyContent : 'space-between',
+        flexDirection: 'row'
     },
 
     titleText : {
