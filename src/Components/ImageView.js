72961f2e3f3247c452f22c7b773e9a2c6b39ccaf
JW-Kim <jw4005.kim@gmail.com>
2018-11-15 14:06:57 +0900

    Create ImageView.js

---
+++ src/Components/ImageView.js
@@ -0,0 +1,42 @@
+import React, {
+    Component
+} from 'react';
+import {
+    View,
+    Dimensions
+} from 'react-native';
+import Image from 'react-native-scalable-image';
+import RNFetchBlob from 'react-native-fetch-blob';
+import Constants from '../Com/Constants.js';
+
+export default class ImageView extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            base64Str : null
+        }
+    }
+
+    componentWillMount () {
+        var cur = this;
+        RNFetchBlob.fetch('GET', 'http://'+Constants.HOST+':'+Constants.PORT+'/product/file/download?fileId='+this.props.fileId)
+          // when response status code is 200
+            .then((res) => {
+                let base64Str = res.base64();
+                let imageBase64 = 'data:image/jpeg;base64,'+base64Str;
+                cur.setState({
+                    base64Str : imageBase64
+                })
+          })
+          // Status code is not 200
+          .catch((errorMessage, statusCode) => {
+            // error handling
+          })
+    }
+
+    render(){
+        return(
+            <Image width={Dimensions.get('window').width} source={{uri :this.state.base64Str}} />
+        )
+    }
+}
