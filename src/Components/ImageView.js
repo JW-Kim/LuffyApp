a87137e60a42049a67a6cdc447a7a8fc58018730
JW-Kim <jw4005.kim@gmail.com>
2018-11-22 16:57:19 +0900

    Update ImageView.js

--- src/Components/ImageView.js
+++ src/Components/ImageView.js
@@ -3,7 +3,8 @@ import React, {
 } from 'react';
 import {
     View,
-    Dimensions
+    Dimensions,
+    AsyncStorage
 } from 'react-native';
 import Image from 'react-native-scalable-image';
 import RNFetchBlob from 'react-native-fetch-blob';
@@ -18,20 +19,26 @@ export default class ImageView extends Component {
     }
 
     componentWillMount () {
+
         var cur = this;
-        RNFetchBlob.fetch('GET', 'http://'+Constants.HOST+':'+Constants.PORT+'/product/file/download?fileId='+this.props.fileId)
-          // when response status code is 200
-            .then((res) => {
-                let base64Str = res.base64();
-                let imageBase64 = 'data:image/jpeg;base64,'+base64Str;
-                cur.setState({
-                    base64Str : imageBase64
-                })
-          })
-          // Status code is not 200
-          .catch((errorMessage, statusCode) => {
-            // error handling
-          })
+        AsyncStorage.getItem('access_token', (err, result) => {
+            RNFetchBlob.fetch('GET', 'http://'+Constants.HOST+':'+Constants.PORT+'/product/file/download?fileId='+this.props.fileId,{
+                'Authorization': 'Bearer '+result
+            })
+              // when response status code is 200
+                .then((res) => {
+                    let base64Str = res.base64();
+                    let imageBase64 = 'data:image/jpeg;base64,'+base64Str;
+                    cur.setState({
+                        base64Str : imageBase64
+                    })
+              })
+              // Status code is not 200
+              .catch((errorMessage, statusCode) => {
+                // error handling
+              })
+
+        })
     }
 
     render(){
