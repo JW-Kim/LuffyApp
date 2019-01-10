45e0f54300b7b2b4c02a0ef776137a8c98d8193e
JW-Kim <jw4005.kim@gmail.com>
2019-01-10 17:37:13 +0900

    NoteDisease 추가

---
+++ src/Components/Note/NoteDiseaseDtl.js
@@ -0,0 +1,26 @@
+import React, {
+    Component
+} from 'react';
+import {
+    View,
+    Text
+} from 'react-native';
+import ModalHeader from '../ModalHeader.js'
+
+export default class NoteStatics extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    componentWillMount () {
+    }
+
+    render(){    
+        <View>
+        </View>
+    }
+}
