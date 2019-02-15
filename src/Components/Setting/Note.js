c0063ea5f1e7428e13e89562347fe2063e286ea3
JWKim <jw4005.kim@gmail.com>
2019-02-15 19:33:10 +0900

    add noteSetting

--- src/Components/Setting/Note.js
+++ src/Components/Setting/Note.js
@@ -5,7 +5,11 @@ import {
     View,
     Text,
 	StyleSheet
+	FlatList,
+	TouchableOpacity,
+	ScrollViewl
 } from 'react-native';
+import ModalHeader from '../ModalHeader.js'
 
 export default class Note extends Component {
 
@@ -17,18 +21,61 @@ export default class Note extends Component {
     }
 
     componentWillMount () {
-    }
+		let myNoteList = [{noteNm:'김재윤'},{noteNm:'김정우'}]    
+
+		this.setState({
+			myNoteList : myNoteList
+		})
+	}
 
     render(){
         return(
-        	<View>
-				<Text>NoteSetting</Text>
+        	<View style={{flex:1, backgroundColor:'#fff'}}>
+				<ModalHeader
+					title='My 일기장'
+					buttonTitle={'닫기'}
+					goEvent={this.closePopup.bind()this}
+				> 
+				</ModalHeader>
+				<View style={{flex:1, padding:10}}>
+					<View style={{flex:0.5}}>
+						<Text style={{fontSize:17}}></Text>						<ScrollView style={{marginTop:10}}>
+							<FlatList
+								data={this.state.myNoteList}
+								keyExtractor={(item, index) => index.toString()}
+								renderItem={({item}) =>
+									<TouchableOpacity activeOpacity={0.9}>
+										<View style={styles.noteItem}>
+											<Text>{item.noteNm}</Text>
+										</View>	
+									</TouchableOpacity>
+								}
+							/>
+						</ScrollView>
+					</View>
+					<View style={{flex:0.5}}>
+						<Text style={{fontSize:17}}>공유 일기장</Text>
+					</View>	
+				</View>
 			</View>   
         )
     }
+
+	closePopup(){
+		this.props.navigation.goBack();
+	}
 }
 
 const styles = StyleSheet.create({
-
-
+	noteItem : {
+		flexDirection : 'row',
+		height : 50,
+		alignItems: 'center',
+		borderLeftWidth: 0.8,
+		borderTopWidth: 0.8,
+		borderRightWidth : 0.8,
+		borderBottomWidth : 0.8,
+		borderColor : '#ebe0eb',
+		paddingLeft: 20
+	}
 })
