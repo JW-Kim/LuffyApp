9c866a2518e17528b7128882a39a345a12a9638a
jwKim <jw4005.kim@gmail.com>
2019-07-04 16:26:11 +0900

    Merge branch 'master' of https://github.com/JW-Kim/LuffyApp
    # Conflicts:
    #	src/Components/Login.js
    #	src/Components/User/UserRegister.js

--- src/Components/Note/NoteDiseaseDtl.js
+++ src/Components/Note/NoteDiseaseDtl.js
@@ -30,8 +30,13 @@ export default class NoteDiseaseDtl extends Component {
             diseaseDt: this.props.navigation.getParam('diseaseDt'),
             diseaseId: this.props.navigation.getParam('diseaseId'),
             saveNoteDiseaseBtnStyle: {backgroundColor: 'gray', height: 60},
-
+            diseaseNmStyle: {borderBottomWidth: 0, borderColor: 'gray'},
+            hospitalNmStyle: {borderBottomWidth: 0, borderColor: 'gray'},
+            prescriptionStyle: {borderWidth: 0, borderColor: 'gray'},
+            symptomStyle: {borderWidth: 0, borderColor: 'gray'}
         }
+
+        let setDisease = this.setDisease.bind(this);
     }
 
     async componentWillMount() {
@@ -129,60 +134,73 @@ export default class NoteDiseaseDtl extends Component {
 
     render() {
         const {navigation} = this.props;
-        const {saveNoteDiseaseBtnStyle} = this.state;
+        const {saveNoteDiseaseBtnStyle, diseaseNmStyle, hospitalNmStyle, prescriptionStyle, symptomStyle} = this.state;
 
         return (
             <View style={{flex: 1, backgroundColor: 'white'}}>
                 <ModalStandardHeader title="질병 작성" navigation={navigation}/>
-                <View style={{height: Dimensions.get('window').height - 208}}>
+                <View style={{height: Dimensions.get('window').height - 140}}>
                     <KeyboardAvoidingView behavior="padding" keyboardVerticalOffset={100} enabled>
                         <ScrollView style={{padding: 10}}>
                             <View style={styles.checkContent}>
-                                <Text style={{width: 70, fontSize: 15, fontWeight: '800'}}>병명</Text>
+                                <View style={styles.rowTextField}><Text style={styles.rowText}>병명</Text></View>
                                 <View style={{
                                     flex: 1,
                                     flexDirection: 'row',
-                                    alignItems: 'center',
-                                    paddingLeft: 50,
-                                    paddingRight: 20
+                                    alignItems: 'center'
                                 }}>
-                                    <TextInput
-                                        style={{flex: 1}}
+                                    <Edit
+                                        style={[{flex: 1}, diseaseNmStyle]}
+                                        height="60"
+                                        underlineColorAndroid="transparent"
+                                        placeholder="영문/숫자 6~12자"
+                                        autoCompleteType="off"
+                                        secureTextEntry={false}
+                                        onFocus={() => this.setState({diseaseNmStyle: {borderBottomWidth: 1, borderColor: 'gray'}})}
+                                        onBlur={() => this.setState({diseaseNmStyle: {borderBottomWidth: 0, borderColor: 'gray'}})}
                                         onChangeText={(diseaseNm) => this.setState({diseaseNm})}
                                         value={this.state.diseaseNm}
-                                    ></TextInput>
+                                    ></Edit>
                                 </View>
                             </View>
                             <View style={[styles.checkContent, {height: 200}]}>
-                                <Text style={{width: 70, fontSize: 15, fontWeight: '800'}}>증상</Text>
+                                <View style={styles.rowTextField}><Text style={styles.rowText}>증상</Text></View>
                                 <View style={{
                                     flex: 1,
                                     flexDirection: 'row',
-                                    alignItems: 'center',
-                                    paddingLeft: 50,
-                                    paddingRight: 20
+                                    alignItems: 'center'
                                 }}>
                                     <TextInput
-                                        style={{flex: 1}}
+                                        style={[{flex: 1}, symptomStyle]}
+                                        numberOfLines={10}
+                                        multiline={true}
+                                        underlineColorAndroid="transparent"
+                                        onFocus={() => this.setState({symptomStyle: {borderWidth: 1, borderColor: 'gray'}})}
+                                        onBlur={() => this.setState({symptomStyle: {borderWidth: 0, borderColor: 'gray'}})}
                                         onChangeText={(symptom) => this.setState({symptom})}
                                         value={this.state.symptom}
                                     ></TextInput>
                                 </View>
                             </View>
                             <View style={styles.checkContent}>
-                                <Text style={{width: 70, fontSize: 15, fontWeight: '800'}}>병원명</Text>
+                                <View style={styles.rowTextField}><Text style={styles.rowText}>병원명</Text></View>
                                 <View style={{
                                     flex: 1,
                                     flexDirection: 'row',
-                                    alignItems: 'center',
-                                    paddingLeft: 50,
-                                    paddingRight: 20
+                                    alignItems: 'center'
                                 }}>
-                                    <TextInput
-                                        style={{flex: 1}}
+                                    <Edit
+                                        style={[{flex: 1}, hospitalNmStyle]}
+                                        height="60"
+                                        underlineColorAndroid="transparent"
+                                        placeholder="영문/숫자 6~12자"
+                                        autoCompleteType="off"
+                                        secureTextEntry={false}
+                                        onFocus={() => this.setState({hospitalNmStyle: {borderBottomWidth: 1, borderColor: 'gray'}})}
+                                        onBlur={() => this.setState({hospitalNmStyle: {borderBottomWidth: 0, borderColor: 'gray'}})}
                                         onChangeText={(hospitalNm) => this.setState({hospitalNm})}
                                         value={this.state.hospitalNm}
-                                    ></TextInput>
+                                    ></Edit>
                                 </View>
                             </View>
                             <View style={[styles.checkContent, {
@@ -190,16 +208,19 @@ export default class NoteDiseaseDtl extends Component {
                                 borderBottomWidth: 0.8,
                                 height: 200
                             }]}>
-                                <Text style={{width: 70, fontSize: 15, fontWeight: '800'}}>처방</Text>
+                                <View style={styles.rowTextField}><Text style={styles.rowText}>처방</Text></View>
                                 <View style={{
                                     flex: 1,
                                     flexDirection: 'row',
-                                    alignItems: 'center',
-                                    paddingLeft: 50,
-                                    paddingRight: 20
+                                    alignItems: 'center'
                                 }}>
                                     <TextInput
-                                        style={{flex: 1}}
+                                        style={[{flex: 1}, prescriptionStyle]}
+                                        numberOfLines={10}
+                                        multiline={true}
+                                        underlineColorAndroid="transparent"
+                                        onFocus={() => this.setState({symptomStyle: {borderWidth: 1, borderColor: 'gray'}})}
+                                        onBlur={() => this.setState({symptomStyle: {borderWidth: 0, borderColor: 'gray'}})}
                                         onChangeText={(prescription) => this.setState({prescription})}
                                         value={this.state.prescription}
                                     ></TextInput>
@@ -212,7 +233,7 @@ export default class NoteDiseaseDtl extends Component {
                     buttonStyle={saveNoteDiseaseBtnStyle}
                     containerViewStyle={{width: '100%', marginLeft: 0, marginRight: 0}}
                     title='저장'
-                    onPress={() => this.insertUser()}/>
+                    onPress={() => this.setDisease()}/>
             </View>
         )
     }
@@ -229,5 +250,20 @@ const styles = StyleSheet.create({
         borderTopWidth: 0.8,
         borderRightWidth: 0.8,
         borderColor: '#ebe0eb'
-    }
+    },
+    textInput: {
+        paddingLeft: 8,
+        paddingRight: 35,
+        height: 60,
+        fontSize: 16,
+        flex: 1
+    },
+    rowTextField: {
+        paddingRight: 20,
+        width: 130,
+        justifyContent: 'center'
+    },
+    rowText: {
+        fontSize: 16
+    },
 })
